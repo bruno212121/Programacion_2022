01fca56a33eed3326d14a1ae68564f14056f4500
BrunoRosales <brunonicorc@hotmail.com>
2022-09-01 13:15:03 -0300

    route admin and user

--- Frontend/main/routes/routes.py
+++ Frontend/main/routes/routes.py
@@ -6,11 +6,27 @@ app = Blueprint('app', __name__, url_prefix='/')
 def index():
     return render_template('main.html')
 
-@app.route('/user_main')
+@app.route('/usuario_main')
 def user_main():
     return render_template('User_main.html')
 
+@app.route('/usuario_main/crear_poema')
+def crear_poema():
+    return render_template('Crear_poema.html')
+    
 @app.route('/admin_main')
 def admin_main():
     return render_template('admin_main.html')
-    
+
+@app.route('/admin_main/crear_usuario')
+def crear_usuario():
+    return render_template('crear_usuario.html')
+
+@app.route('/admin_main/eliminar_usuario')
+def eliminar_usuario():
+    return render_template('eliminar_usuario.html')
+
+@app.route('login')
+def login():
+    return render_template('login.html')
+
